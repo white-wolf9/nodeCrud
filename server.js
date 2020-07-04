57e75eac9e399d4226197608e637a3402620f15a
SauravSanyal\Saurav Sanyal <saurav.sanyal@fluentcommerce.com>
2020-07-05 01:26:04 +0530

    Hello World

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log('May the node be with me!')
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log('Frankinstien !! It is alive!! @ 3000')
+})
+
+app.get('/', function (req,res) {
+    res.send('Hello World!')
+})
+
